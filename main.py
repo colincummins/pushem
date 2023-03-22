2c0c7de836fb82f9927c5698b9cf2487337a829a
Colin Cummins <all.our.august.sundays@gmail.com>
2023-03-21 21:00:21 -0700

    Implement get_row_col() in main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pygame
-from pushem.constants import WIDTH, HEIGHT
+from pushem.constants import WIDTH, HEIGHT, SQUARE_SIZE
 from pushem.board import Board
-WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 60
 pygame.display.set_caption("PushEm")
+
+
+def get_row_col(pos: (int, int)) -> (int, int):
+    """
+    Get grid row and column from mouse position
+    :param pos: mouse pos from pygame
+    :return: row and column (int, int)
+    """
+    return pos[1]//SQUARE_SIZE, pos[0]//SQUARE_SIZE
 
 
 def main():
@@ -22,16 +32,13 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                print(get_row_col(pygame.mouse.get_pos()))
 
         board.draw_grid(WIN)
         board.draw_pieces(WIN)
         pygame.display.update()
 
-
     pygame.quit()
 
+
 main()
-
-
-
