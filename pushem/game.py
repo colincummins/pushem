9a2366b27365290cf3a75d83eb97e10e1fe938d2
Colin Cummins <all.our.august.sundays@gmail.com>
2023-12-31 18:46:05 -0800

    WIP - Menu Main menu now proceeds to play when you hit "play" button

--- pushem/game.py
+++ pushem/game.py
@@ -14,6 +14,12 @@ class Game:
         pygame.display.set_caption("PushEm")
         self.mode = "main_menu"
 
+    def get_mode(self):
+        return self.mode
+
+    def set_mode(self, mode):
+        self.mode = mode
+
     def get_row_col(self, pos: (int, int)) -> (int, int):
         """
         Get grid row and column from mouse position
@@ -23,6 +29,7 @@ class Game:
         return pos[1] // SQUARE_SIZE, pos[0] // SQUARE_SIZE
 
     def resume_play(self):
+        print("resuming play")
         self.mode = "play"
 
     def run_game(self):
@@ -38,11 +45,11 @@ class Game:
         clock = pygame.time.Clock()
         board = Board(first_player)
 
-        announce_first = pygame_menu.Menu('First Player', WIDTH / 2, HEIGHT / 2, theme=pygame_menu.themes.THEME_BLUE, onclose=self.resume_play())
+        announce_first = pygame_menu.Menu('First Player', WIDTH / 2, HEIGHT / 2, theme=pygame_menu.themes.THEME_BLUE)
         announce_first.add.button('OK', pygame_menu.events.CLOSE)
 
         main_menu = pygame_menu.Menu('PushEm', WIDTH / 2, HEIGHT / 2, theme=pygame_menu.themes.THEME_BLUE)
-        main_menu.add.button('Play', announce_first)
+        main_menu.add.button('Play', self.set_mode, "play")
         main_menu.add.button('Quit', pygame_menu.events.EXIT)
 
         """Main logic/rendering loop"""
@@ -66,7 +73,9 @@ class Game:
             board.draw_grid(self.WIN)
             board.draw_pieces(self.WIN)
 
-            if main_menu.is_enabled():
+            print(self.mode)
+
+            if main_menu.is_enabled() and self.mode == "main_menu":
                 main_menu.draw(self.WIN)
                 main_menu.update(events)
 
